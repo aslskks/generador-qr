1bda6fbd2a43d23e17289dcb3be88f8e2cbf9b80
aslskks <130728002+aslskks@users.noreply.github.com>
2023-10-06 17:01:31 -0600

    code

---
+++ app.py
@@ -0,0 +1,64 @@
+import qrcode
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+
+def generate_qr():
+    data_type = data_type_var.get()
+    data = data_entry.get()
+    filename = filename_entry.get()
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img_filename = f'{filename}_qrcode.png'
+    img.save(img_filename)
+
+    img = Image.open(img_filename)
+    img.thumbnail((300, 300))
+    img = ImageTk.PhotoImage(img)
+    qr_label.config(image=img)
+    qr_label.image = img
+
+
+# Crear la ventana de la aplicación
+app = tk.Tk()
+app.title("Generador de Códigos QR")
+
+data_type_var = tk.StringVar(value="url")
+
+data_label = tk.Label(app, text="Datos:")
+data_label.pack()
+
+data_entry = tk.Entry(app)
+data_entry.pack()
+
+filename_label = tk.Label(app, text="Nombre del archivo:")
+filename_label.pack()
+
+filename_entry = tk.Entry(app)
+filename_entry.pack()
+
+data_type_radio_url = tk.Radiobutton(
+    app, text="URL", variable=data_type_var, value="url")
+data_type_radio_url.pack()
+
+data_type_radio_text = tk.Radiobutton(
+    app, text="Texto", variable=data_type_var, value="text")
+data_type_radio_text.pack()
+
+generate_button = tk.Button(app, text="Generar QR", command=generate_qr)
+generate_button.pack()
+
+qr_label = tk.Label(app)
+qr_label.pack()
+
+app.mainloop()
